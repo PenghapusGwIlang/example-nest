6c15a93311c7d52840dca512c013b85a3ee42010
Muhammad Naufal <naufalmhd24@gmail.com>
2023-10-07 20:38:19 +0700

    uses the serialize interceptor to be globally in User Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,10 +14,10 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('users')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @Serialize(UserDto)
   @Get()
   findAllUsers() {
     return this.usersService.find();
@@ -28,7 +28,6 @@ export class UsersController {
     return this.usersService.create(body.name, body.email, body.password);
   }
 
-  @Serialize(UserDto)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOneBy(parseInt(id));
