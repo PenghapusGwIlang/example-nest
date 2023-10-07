6da3b483cb6c201a7dd5294e2d043c3a83adf1cd
Muhammad Naufal <naufalmhd24@gmail.com>
2023-10-07 15:46:16 +0700

    implement find method to controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -6,8 +6,23 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @Get()
+  findAllUsers() {
+    return this.usersService.find();
+  }
+
   @Post()
   createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body.name, body.email, body.password);
+  }
+
+  @Get()
+  findUsers() {
+    return this.usersService.find();
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOneBy(parseInt(id));
   }
 }
