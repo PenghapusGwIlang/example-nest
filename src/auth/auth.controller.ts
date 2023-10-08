50910385dd579b4d72ed4ccdf73afaa1c48125d8
Muhammad Naufal <naufalmhd24@gmail.com>
2023-10-08 15:17:04 +0700

    created a custom decorator for current user and the interceptor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { UsersService } from 'src/users/users.service';
 import { LoginUserDto } from './dtos/login-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from 'src/users/dtos/user.dto';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -38,7 +39,7 @@ export class AuthController {
   }
 
   @Get('/whoami')
-  async whoAmI(@Session() session: any) {
-    return this.usersService.findOneBy(session.userId);
+  async whoAmI(@CurrentUser() user: string) {
+    return user;
   }
 }
