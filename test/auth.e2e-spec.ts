38826a6fa38f683df4a799a836f099ae272a2284
Muhammad Naufal <naufalmhd24@gmail.com>
2023-10-13 20:38:53 +0700

    test: add auth controller e2e test

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { setupApp } from '../src/setup-app';
+
+describe('Auth Controller (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    setupApp(app);
+    await app.init();
+  });
+
+  it('handles register', () => {
+    return request(app.getHttpServer())
+      .post('/auth/register')
+      .send({
+        name: 'bopal',
+        email: 'bopal2@mail.com',
+        password: 'password',
+      })
+      .expect(201)
+      .then(({ body }: request.Response) => {
+        expect(body.id).toBeDefined();
+        expect(body.name).toBe('bopal');
+        expect(body.email).toBe('bopal2@mail.com');
+      });
+  });
+});
